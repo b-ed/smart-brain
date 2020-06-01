1fd578b1ea99ed9ffd0256266e6cb6dc220ed3df
b-ed <67429249+b-ed@users.noreply.github.com>
2020-06-01 09:51:06 +0300

    removing undefined variable

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json(database.users)
+    res.json('it is working')
 })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
